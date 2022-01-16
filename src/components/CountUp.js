197dc268a525ad57752d5317f7044e2e5670be75
Jason Hargrove <rijaha@gmail.com>
2022-01-15 22:17:26 -0600

    Added Hello function.

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -1,7 +1,14 @@
 let count = Math.random();
 
+function Hello () {
+    return <div>Hello React!</div>
+}
+
 export function Add () {
     return (
+        <>
         <h1>{count}</h1>
+        <Hello />
+        </>
     )
 }
