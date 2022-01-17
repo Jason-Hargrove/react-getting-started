08caf6451b1b9f14be3a840a4caee260cc93892f
Jason Hargrove <rijaha@gmail.com>
2022-01-16 21:41:31 -0600

    Updated button with an inline onClick to console.log a random number.

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 let count = Math.random();
 
 function Button () {
-    return <button>TEST</button>
+    // a, b = useState()
+    // a) state object (getter)
+    // b) updater function (setter)
+    const [counter, setCounter] = useState(0)
+
+    return <button onClick={() => console.log(Math.random())}>{counter}</button>
 }
 
 export function Add () {
