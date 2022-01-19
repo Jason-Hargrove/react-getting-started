088a830cb753b19b217f743e8fb6cf0182d4f9c0
Jason Hargrove <rijaha@gmail.com>
2022-01-18 20:40:21 -0600

    Destructuring.

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -19,6 +19,23 @@ function Display({ message }) {
     );
 }
 
+// Destructuring
+
+const {PI, E, SQRT2} = Math;
+
+const circle = {
+    label: 'circleX',
+    radius: 2,
+};
+
+const CircleArea = ({radius}, {precision = 2} = {}) =>
+    (PI * radius * radius).toFixed(precision);
+
+console.log(CircleArea(circle, { precision: 5 }));
+
+// Destructuring Arrays
+
+
 function App() {
     const [counter, setCounter] = useState(42);
     const incrementCounter = () => setCounter(counter*2);
@@ -27,6 +44,7 @@ function App() {
             <h1>{count}</h1>
             <Button increment={incrementCounter} />
             <Display message={counter}/>
+            <CircleArea radius={circle.radius} />
         </>
     );
 }
