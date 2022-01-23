22cfc87bcfc3f48fca3968a87cae9ee8e977eabe
Jason Hargrove <rijaha@gmail.com>
2022-01-22 20:47:26 -0600

    Added styling to card app.

--- src/components/GitHubCardApp.js
+++ src/components/GitHubCardApp.js
@@ -1,16 +1,19 @@
+import './GitHubCardApp.css';
+
 const GitHubCardApp = ({ title }) => {
     return (
         <>
-        <div className="header">{title}</div>
+            <div className="header">
+                {title}
+            </div>
         </>
-    )
-    
-};
+    );   
+}
 
 export function CardApp() {
     return (
         <>
-            <GitHubCardApp title="Hi" />
+            <GitHubCardApp title="The GitHub Card App" />
         </>
     );  
 }
