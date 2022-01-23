4d40103ffb2b3b4cadbca35e9a204e2ba413d6cf
Jason Hargrove <rijaha@gmail.com>
2022-01-23 14:58:09 -0600

    Random Color using JavaSricpt styles.

--- src/components/GitHubCardApp.js
+++ src/components/GitHubCardApp.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import './GitHubCardApp.css';
+// import './GitHubCardApp.css';
+
+// Random Color JavaScript style componet.
+class ConditionalStyle extends React.Component {
+    render() {
+        return (
+            <div style={{ color: Math.random() < 0.5 ? 'green' : 'red' }}>
+                How do you like this?
+            </div>
+        );
+    }
+}
 
 class Card extends React.Component {
     render() {
         return (
-            <div className='github-profile'>
+            <div className='github-profile' style={{ margin: '1rem' }}>
                 <img src="https://placehold.it/75" />
-                <div className="info">
-                    <div className='name'>Name here...</div>
+                <div className="info" style={{ display: 'inline-block', marginLeft: 10 }}>
+                    <div className='name' style={{ fontSize: '125%'}}>Name here...</div>
                     <div className='company'>Company here...</div>
                 </div>
             </div>
@@ -24,6 +35,7 @@ class GitHubCardApp extends React.Component {
             <div>
                 <div className="header">{this.props.title}</div>
                 <Card />
+                <ConditionalStyle />
             </div>
         );
     }
