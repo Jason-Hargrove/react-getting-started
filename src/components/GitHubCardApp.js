e6a1a73aa4d18328efa6d385bb032ad9b917d91e
Jason Hargrove <rijaha@gmail.com>
2022-01-22 21:07:31 -0600

    Added styling.

--- src/components/GitHubCardApp.js
+++ src/components/GitHubCardApp.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './GitHubCardApp.css';
 
+class Card extends React.Component {
+    render() {
+        return (
+            <div className='github-profile'>
+                <img src="https://placehold.it/75" />
+                <div className="info">
+                    <div className='name'>Name here...</div>
+                    <div className='company'>Company here...</div>
+                </div>
+            </div>
+        );
+    }
+}
+
 // Class Component
 class GitHubCardApp extends React.Component {
     // constructor
     // this
     render() {
-        return <div className="header">{this.props.title}</div>
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Card />
+            </div>
+        );
     }
 }
 
